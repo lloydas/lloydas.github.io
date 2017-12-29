115425b19ec6757dbf28b7aae5e887bf523f1ead
andrewlloyd04 <lloydas@umich.edu>
2017-12-28 22:54:24 -0500

    updates

--- frames/views/after_login.py
+++ frames/views/after_login.py
@@ -19,23 +19,14 @@ def get_credentials():
     c.perform()
     d = json.loads(b.getvalue())
 
-    if d['aud'] != 'amzn1.application-oa2-client.5815d5058fb444c0b93783a79adec01d' :
-    # the access token does not belong to us
-    raise BaseException("Invalid Token")
+    # if d['aud'] != 'amzn1.application-oa2-client.5815d5058fb444c0b93783a79adec01d' :
+    #     # the access token does not belong to us
+    #     raise BaseException("Invalid Token")
+    
+    return flask.render_template("after_login.html", **d)
 
-    # exchange the access token for user profile
-    b = StringIO.StringIO()
 
-    c = pycurl.Curl()
-    c.setopt(pycurl.URL, "https://api.amazon.com/user/profile")
-    c.setopt(pycurl.HTTPHEADER, ["Authorization: bearer " + access_token])
-    c.setopt(pycurl.SSL_VERIFYPEER, 1)
-    c.setopt(pycurl.WRITEFUNCTION, b.write)
-
-    c.perform()
-    d = json.loads(b.getvalue())
-
-    print "%s %s %s"%(d['name'], d['email'], d['user_id'])
+    # print "%s %s %s"%(d['name'], d['email'], d['user_id'])
         
         
         
