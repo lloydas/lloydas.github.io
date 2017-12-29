2f717d4aea32df3b51074a62bb92f570c39bc89d
andrewlloyd04 <lloydas@umich.edu>
2017-12-28 22:32:45 -0500

    updates

---
+++ frames/views/after_login.py
@@ -0,0 +1,43 @@
+import frames
+import pycurl
+import urllib
+import json
+import StringIO
+import flask
+from flask import request, session, redirect, url_for
+
+@frames.app.route('/html/create/', methods=['GET', 'POST'])
+def get_credentials():
+    b = StringIO.StringIO()
+
+    # verify that the access token belongs to us
+    c = pycurl.Curl()
+    c.setopt(pycurl.URL, "https://api.amazon.com/auth/o2/tokeninfo?access_token=" + urllib.quote_plus(access_token))
+    c.setopt(pycurl.SSL_VERIFYPEER, 1)
+    c.setopt(pycurl.WRITEFUNCTION, b.write)
+
+    c.perform()
+    d = json.loads(b.getvalue())
+
+    if d['aud'] != 'amzn1.application-oa2-client.5815d5058fb444c0b93783a79adec01d' :
+    # the access token does not belong to us
+    raise BaseException("Invalid Token")
+
+    # exchange the access token for user profile
+    b = StringIO.StringIO()
+
+    c = pycurl.Curl()
+    c.setopt(pycurl.URL, "https://api.amazon.com/user/profile")
+    c.setopt(pycurl.HTTPHEADER, ["Authorization: bearer " + access_token])
+    c.setopt(pycurl.SSL_VERIFYPEER, 1)
+    c.setopt(pycurl.WRITEFUNCTION, b.write)
+
+    c.perform()
+    d = json.loads(b.getvalue())
+
+    print "%s %s %s"%(d['name'], d['email'], d['user_id'])
+        
+        
+        
+        
+        
